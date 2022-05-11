9456c128211ce34e96185900b110fb54dac7f82f
Jimmy Tran <jimmy.tran@polyconseil.fr>
2022-05-11 10:46:45 +0200

    Add notification service

---  src/main/java/WorkflowService.java
+++  src/main/java/WorkflowService.java
@@ -2,15 +2,20 @@ public class WorkflowService {
 
   private final InventoryService inventoryService;
   private final TicketingService ticketingService;
+  private final NotificationService notificationService;
 
-  public WorkflowService(InventoryService inventoryService, TicketingService ticketingService) {
+  public WorkflowService(InventoryService inventoryService, TicketingService ticketingService,
+      NotificationService notificationService) {
     this.inventoryService = inventoryService;
     this.ticketingService = ticketingService;
+    this.notificationService = notificationService;
   }
 
   public void run(int numberOfSeats) {
     if(inventoryService.decrementCapacity(numberOfSeats)) {
       ticketingService.printTicket(numberOfSeats);
+    } else {
+      notificationService.notifyCustomer();
     }
   }
 }
---  src/test/java/E2ETest.java
+++  src/test/java/E2ETest.java
@@ -9,13 +9,14 @@ class E2ETest {
   private TicketingService ticketingService;
   private WorkflowService workflowService;
   private RequestBookingService requestBookingService;
+  private NotificationService notificationService;
 
   @BeforeEach
   void setUp() {
-
     inventoryService = new InventoryService(5);
     ticketingService = new TicketingService();
-    workflowService = new WorkflowService(inventoryService, ticketingService);
+    notificationService = new NotificationService();
+    workflowService = new WorkflowService(inventoryService, ticketingService, notificationService);
     requestBookingService = new RequestBookingService(workflowService);
   }
 
@@ -24,5 +25,11 @@ class E2ETest {
     requestBookingService.book(3);
 
     assertEquals(inventoryService.capacity, 2);
+  }
+  @Test
+  public void KO() {
+    requestBookingService.book(6);
+
+    assertEquals(inventoryService.capacity, 5);
   }
 }
